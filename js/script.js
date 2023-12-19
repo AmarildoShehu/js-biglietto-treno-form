2a07fb88de134e1bb2aa8620e4a31ea38d329a9a
Amarildo <amarildos@outloo.it>
2023-12-19 18:51:42 +0100

    added final price and the result

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // collego il file js.
 console.log('JS OK');
 
+// chiedo un controllo di tutti gli elementi prima del click sul bottone.
+document.getElementById('calcolaButton').addEventListener('click', function() {
 // chiedo nome e cognome utente.
 
 const nome = document.getElementById('nomeInput').value.trim();
@@ -14,17 +16,23 @@ const eta = parseInt(document.getElementById('etaInput').value);
 // Controllo se l'utente ha inserito i dati correttamente
 if (nome.length < 3 || cognome.length < 3) {
     alert('Inserisci un nome e un cognome validi (almeno 3 caratteri).');
-     return;
+    return;
 }
 if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
      alert('Inserisci valori validi per i chilometri e l\'età.');
-     return;
+    return;
 }
 
 // Calcolo prezzo con lo sconto
 const prezzoBase = km * 0.21;
-if (eta < 18) {
-    prezzoBase *= 0.8;
-} else if (eta >= 65) {
-    prezzoBase *= 0.6;
-}
+    if (eta < 18) {
+        prezzoBase *= 0.8;
+    } else if (eta >= 65) {
+        prezzoBase *= 0.6;
+    }
+
+    const prezzoFinale = prezzoBase.toFixed(2);
+
+    const risultato = `Ciao ${nome} ${cognome}, il prezzo del viaggio è di € ${prezzoFinale}`;
+    document.getElementById('risultato').innerText = risultato;
+});
