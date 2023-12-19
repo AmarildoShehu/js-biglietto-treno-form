377ccfab69b0bfd4fd7890f87053271bd8f76ab7
Amarildo <amarildos@outloo.it>
2023-12-19 17:54:38 +0100

    checking the data

--- js/script.js
+++ js/script.js
@@ -10,4 +10,13 @@ const cognome = document.getElementById('cognomeInput').value.trim();
 
 const km = parseFloat(document.getElementById('kmInput').value);
 const eta = parseInt(document.getElementById('etaInput').value);
-    
+  
+// Controllo se l'utente ha inserito i dati correttamente
+if (nome.length < 3 || cognome.length < 3) {
+    alert('Inserisci un nome e un cognome validi (almeno 3 caratteri).');
+     return;
+}
+if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
+     alert('Inserisci valori validi per i chilometri e l\'età.');
+     return;
+}
